48f95ff6b75f4c0cd9c1fa14d3e7a6d35bb135bd
meohin <meohin.meo@gmail.com>
2024-09-06 13:46:19 +0900

    Initial project setup

---
+++ timetrainer1/src/main/java/com/meohin/timetrainer1/Timetrainer1Application.java
@@ -0,0 +1,13 @@
+package com.meohin.timetrainer1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Timetrainer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Timetrainer1Application.class, args);
+	}
+
+}
---
+++ timetrainer1/src/test/java/com/meohin/timetrainer1/Timetrainer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meohin.timetrainer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Timetrainer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

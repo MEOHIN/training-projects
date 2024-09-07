eda92f2ee665568a47cdf2808cb599090dc50119
meohin <meohin.meo@gmail.com>
2024-09-07 15:51:33 +0900

    feat: 루트 경로 리다이렉션을 위한 MainController 추가

---
+++ timetrainer1/src/main/java/com/meohin/timetrainer1/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.meohin.timetrainer1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root() {
+        return "redirect:/task/list";
+    }
+}

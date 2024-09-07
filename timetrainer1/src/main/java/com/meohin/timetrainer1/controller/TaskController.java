25a655e62a9c978736b489d1d49b400e9b4b8fc6
meohin <meohin.meo@gmail.com>
2024-09-07 15:53:17 +0900

    feat: 태스크 관련 요청 처리를 위한 TaskController 구현

---
+++ timetrainer1/src/main/java/com/meohin/timetrainer1/controller/TaskController.java
@@ -0,0 +1,28 @@
+package com.meohin.timetrainer1.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.meohin.timetrainer1.entity.Task;
+import com.meohin.timetrainer1.service.TaskService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    private final TaskService taskService;
+    
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Task> taskList = taskService.getTaskList();
+        model.addAttribute("task_list", taskList);
+        return "task_list";
+    }
+}

7f16d74bee683947b61359eee0ecd548afa66fee
meohin <meohin.meo@gmail.com>
2024-09-13 18:35:03 +0900

    feat: 회원가입 기능 추가
    - `GET /sign/up` 핸들러 추가: 회원가입 페이지로 이동
    - `POST /sign/up` 핸들러 추가: 회원 정보 처리 및 리다이렉트
    - `@RequestParam`을 사용하여 회원 정보를 받아 POST 요청 처리 (TODO: 추가 구현 필요)

--- timetrainer1/src/main/java/com/meohin/timetrainer1/controller/SignController.java
+++ timetrainer1/src/main/java/com/meohin/timetrainer1/controller/SignController.java
@@ -2,7 +2,10 @@ package com.meohin.timetrainer1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/sign")
@@ -12,5 +15,17 @@ public class SignController {
     public String signin() {
         return "/sign/sign_in";
     }
-    
+
+    @GetMapping("/up")
+    public String signup() {
+        return "/sign/sign_up";
+    }
+
+    @PostMapping("/up")
+    public String signup(@RequestParam String user) {
+        //TODO: process POST request
+        
+        
+        return "redirect:/task/list";
+    }
 }
